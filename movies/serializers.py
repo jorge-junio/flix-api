d4e6acbc272d9f1c7f51d76d8d36cf0061c14ec2
Jorge Silva <jjunio.profissional@gmail.com>
2026-04-09 16:30:36 -0300

    Add SerializerMethodField em MovieModelSerializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 from genres.models import Genre
@@ -5,19 +6,28 @@ from actors.models import Actor
 
 
 class MovieModelSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(ready_only=True)
 
     class Meta:
         model = Movie
         fields = '__all__'
 
+    def get_rate(self, obj):
+        rate = obj.reviews.agregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return round(rate, 1)
+
+        return None
+
     def validate_release_date(self, value):
-        if value.year < 1990:
+        if value.year < 1900:
             raise serializers.ValidationError(
                 'A data de lançamento não pode ser anterior a 1990.')
         return value
 
     def validate_resume(self, value):
-        if len(value) > 200:
+        if len(value) > 500:
             raise serializers.ValidationError(
                 'Resumo não deve ser maior do que 200 caracteres.')
         return value
