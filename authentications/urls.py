5b16dccd9665727e469bd056fea51a31825d859d
Jorge Silva <jjunio.profissional@gmail.com>
2026-04-09 18:34:59 -0300

    Add rotas de restartar o token e de verificar o token

--- authentications/urls.py
+++ authentications/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView, TokenVerifyView)
 
 
 urlpatterns = [
     path('authentications/tokens/', TokenObtainPairView.as_view(),
          name='token_obtain_pair'),
+    path('authentications/tokens/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('authentications/tokens/verify/', TokenVerifyView.as_view(),
+         name='token_verify'),
 ]
