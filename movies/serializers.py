aef0e5df3d2ae472664927cb02496b4bf8441290
Jorge Silva <jjunio.profissional@gmail.com>
2026-04-09 15:46:46 -0300

    Cria funções de validação de campos no serializer de Movie

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,9 +1,41 @@
 from rest_framework import serializers
 from movies.models import Movie
+from genres.models import Genre
+from actors.models import Actor
 
 
-class MovieSerializer(serializers.Serializer):
+class MovieModelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError(
+                'A data de lançamento não pode ser anterior a 1990.')
+        return value
+
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError(
+                'Resumo não deve ser maior do que 200 caracteres.')
+        return value
+
+
+# exemplo de como ficaria o serializer manual. Ele não permite criar ou
+# atualizar dados pois os métodos não estão implementados informando onde
+# salvar. Esse tipo de Serializer da forma que está só serve para transcrição
+# de dados depois que já pegou no banco.
+class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField()
+    genre = serializers.PrimaryKeyRelatedField(
+        queryset=Genre.objects.all(),
+    )
+    release_date = serializers.DateField()
+    actors = serializers.PrimaryKeyRelatedField(
+        queryset=Actor.objects.all(),
+        many=True,
+    )
+    resume = serializers.CharField()
